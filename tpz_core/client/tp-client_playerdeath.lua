cc7e46f82a51cea0c915a90cda809ea52598bc67
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-21 00:20:25 +0300

    Update tp-client_playerdeath.lua

--- tpz_core/client/tp-client_playerdeath.lua
+++ tpz_core/client/tp-client_playerdeath.lua
@@ -22,47 +22,45 @@ AddEventHandler('onResourceStop', function(resourceName)
     for index, effect in pairs (Config.OnPlayerDeath.DeathCameraEffects) do
       AnimpostfxStop(effect)
     end
-    
   
   end
   
-
 end)
 
 RegisterDeathPromptActions = function()
 
-    for index, tprompt in pairs (Config.OnPlayerDeath.PromptKeys) do
+  for index, tprompt in pairs (Config.OnPlayerDeath.PromptKeys) do
 
-      if tprompt.enabled then
+    if tprompt.enabled then
 
-        -- We are getting the label of the prompt and the key.
-        local str      = tprompt.label
-        local keyPress = tprompt.key
-      
-        -- We registering the prompt.
-        local dPrompt = PromptRegisterBegin()
-        PromptSetControlAction(dPrompt, keyPress)
-        str = CreateVarString(10, 'LITERAL_STRING', str)
-        PromptSetText(dPrompt, str)
+      -- We are getting the label of the prompt and the key.
+      local str      = tprompt.label
+      local keyPress = tprompt.key
+    
+      -- We registering the prompt.
+      local dPrompt = PromptRegisterBegin()
+      PromptSetControlAction(dPrompt, keyPress)
+      str = CreateVarString(10, 'LITERAL_STRING', str)
+      PromptSetText(dPrompt, str)
 
-        -- When registering, we set the respawn key as not enabled since we use a cooldown system.
-        if index == 'RESPAWN' then
-          PromptSetEnabled(dPrompt, 0)
-        else
-          PromptSetEnabled(dPrompt, 1)
-        end
-
-        PromptSetVisible(dPrompt, 1)
-        PromptSetStandardMode(dPrompt, 1)
-        PromptSetHoldMode(dPrompt, tprompt.holdMode) -- we set the hold mode
-        PromptSetGroup(dPrompt, Prompts)
-        Citizen.InvokeNative(0xC5F428EE08FA7F2C, dPrompt, true)
-        PromptRegisterEnd(dPrompt)
-      
-        table.insert(PromptsList, {prompt = dPrompt, action = index})
+      -- When registering, we set the respawn key as not enabled since we use a cooldown system.
+      if index == 'RESPAWN' then
+        PromptSetEnabled(dPrompt, 0)
+      else
+        PromptSetEnabled(dPrompt, 1)
       end
 
+      PromptSetVisible(dPrompt, 1)
+      PromptSetStandardMode(dPrompt, 1)
+      PromptSetHoldMode(dPrompt, tprompt.holdMode) -- we set the hold mode
+      PromptSetGroup(dPrompt, Prompts)
+      Citizen.InvokeNative(0xC5F428EE08FA7F2C, dPrompt, true)
+      PromptRegisterEnd(dPrompt)
+    
+      table.insert(PromptsList, {prompt = dPrompt, action = index})
     end
+
+  end
 
 end
 
@@ -86,7 +84,6 @@ end)
 
 RegisterNetEvent('tpz_core:onPlayerRespawn')
 AddEventHandler('tpz_core:onPlayerRespawn', function()
-  TriggerServerEvent('tpz_core:onPlayerDeathContents')
   DeathData.cooldown = 0
 end)
 
@@ -98,7 +95,7 @@ end)
 -- Registering the death prompt actions.
 Citizen.CreateThread(function()
 
- RegisterDeathPromptActions()
+  RegisterDeathPromptActions() 
 
   while true do
 
@@ -123,8 +120,6 @@ Citizen.CreateThread(function()
 
         DeathData.cooldown = Config.OnPlayerDeath.PromptKeys['RESPAWN'].cooldown
 
-        -- create camera?
-        
         if Config.OnPlayerDeath.DeathCamera then
           StartDeathCam()
         end
@@ -149,7 +144,7 @@ Citizen.CreateThread(function()
           notifyText = Config.OnPlayerDeath.BottomPromptLabelDisplays['WHILE_NO_RESPAWN_COOLDOWN']
         end
 
-        local label = CreateVarString(10, 'LITERAL_STRING', notifyText )
+        local label = CreateVarString(10, 'LITERAL_STRING', notifyText)
         PromptSetActiveGroupThisFrame(Prompts, label)
 
         for index, prompt in pairs(PromptsList) do
@@ -158,7 +153,6 @@ Citizen.CreateThread(function()
 
             if DeathData.cooldown <= 0 then
               PromptSetEnabled(prompt.prompt, 1)
-            
             else
 
               PromptSetEnabled(prompt.prompt, 0)
@@ -172,14 +166,7 @@ Citizen.CreateThread(function()
 
               DoScreenFadeOut(3000)
 
-              -- tpz_metabolism
-              TriggerEvent("tpz_metabolism:setMetabolismValue", "HUNGER", "add", 100)
-              TriggerEvent("tpz_metabolism:setMetabolismValue", "THIRST", "add", 100)
-
-              TriggerEvent("tpz_metabolism:setMetabolismValue", "STRESS", "remove", 100)
-              TriggerEvent("tpz_metabolism:setMetabolismValue", "ALCOHOL", "remove", 100)
-
-
+              Wait(3000)
               OnPlayerRespawn()
 
             end
@@ -228,8 +215,6 @@ end)
 
 OnPlayerRespawn = function()
 
-  TriggerEvent('tpz_core:onPlayerRespawn')
-
   local player    = PlayerPedId()
   local pedCoords = GetEntityCoords(player)
 
@@ -239,17 +224,16 @@ OnPlayerRespawn = function()
 
   for _, location in pairs(Config.OnPlayerDeath.RespawnLocations) do
 
-      local locationCoords = vector3(location.Coords.x, location.Coords.y, location.Coords.z)
-      local currentDistance = #(pedCoords - locationCoords)
+    local locationCoords = vector3(location.Coords.x, location.Coords.y, location.Coords.z)
+    local currentDistance = #(pedCoords - locationCoords)
 
-      if currentDistance <= closestDistance then
-          closestDistance = currentDistance
-          closestLocation = location.Town
-          spawnedCoords   = location.Coords
-      end
+    if currentDistance <= closestDistance then
+      closestDistance = currentDistance
+      closestLocation = location.Town
+      spawnedCoords   = location.Coords
+    end
+
   end
-
-  --SetEntityHealth(player, Config.HealthOnRespawn)
 
   ResurrectPlayer(spawnedCoords, false)
  -- TriggerServerEvent("tpzcore:getPlayerSkin")
@@ -277,16 +261,15 @@ ResurrectPlayer = function(currentHospital, hasBeenRevived)
 
   end
 
+  TriggerServerEvent('tpz_core:onPlayerDeathContents')
   TriggerServerEvent('tpz_core:savePlayerDeathStatus', 0)
 
   DeathData.isDead = false
 
-  DisplayRadar(true)
-
   --setPVP()
 
   if currentHospital then -- set entitycoords with heading
-      Citizen.InvokeNative(0x203BEFFDBE12E96A, player, currentHospital.x, currentHospital.y, currentHospital.z, currentHospital.heading, false, false, false)
+    Citizen.InvokeNative(0x203BEFFDBE12E96A, player, currentHospital.x, currentHospital.y, currentHospital.z, currentHospital.heading, false, false, false)
   end
 
   Wait(2000)
@@ -296,21 +279,17 @@ ResurrectPlayer = function(currentHospital, hasBeenRevived)
     keepdown = true
     CreateThread(function() -- tread to keep player down
         while keepdown do
-            Wait(0)
-            SetPedToRagdoll(PlayerPedId(), 4000, 4000, 0, 0, 0, 0)
-            ResetPedRagdollTimer(PlayerPedId())
+          Wait(0)
+          SetPedToRagdoll(PlayerPedId(), 4000, 4000, 0, 0, 0, 0)
+          ResetPedRagdollTimer(PlayerPedId())
         end
     end)
-
          
     local innerHealth = Citizen.InvokeNative(0x36731AC041289BB1, PlayerPedId(), 0)
 
     if innerHealth then
       SetEntityHealth(PlayerPedId(), Config.OnPlayerDeath.HealthOnRespawn + innerHealth)
     end
-
-    --TriggerEvent("vorpmetabolism:setValue", "Thirst", 1000)
-    -- TriggerEvent("vorpmetabolism:setValue", "Hunger", 1000)
 
     if Config.OnPlayerDeath.PlayAnimPostFXPlay then
       AnimpostfxPlay("Title_Gen_FewHoursLater")
@@ -324,23 +303,31 @@ ResurrectPlayer = function(currentHospital, hasBeenRevived)
       AnimpostfxPlay("PlayerWakeUpInterrogation") 
     end
 
-    Wait(19000)
+    DisplayRadar(true)
+
+    Wait(Config.OnPlayerDeath.RagdollKeepDown)
     keepdown = false
 
   else
-      DoScreenFadeIn(2000) 
+    DoScreenFadeIn(2000) 
 
-      local innerHealth = Citizen.InvokeNative(0x36731AC041289BB1, PlayerPedId(), 0)
+    local innerHealth = Citizen.InvokeNative(0x36731AC041289BB1, PlayerPedId(), 0)
 
-      if innerHealth then
-        SetEntityHealth(PlayerPedId(), Config.OnPlayerDeath.HealthOnRespawn + innerHealth)
-      end
+    if innerHealth then
+      SetEntityHealth(PlayerPedId(), Config.OnPlayerDeath.HealthOnRespawn + innerHealth)
+    end
 
-    --  TriggerEvent("vorpmetabolism:setValue", "Thirst", 1000)
-    --  TriggerEvent("vorpmetabolism:setValue", "Hunger", 1000)
   end
 
-  
+  TriggerEvent('tpz_core:onPlayerRespawn')
+
+  -- tpz_metabolism
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "HUNGER", "add", 100)
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "THIRST", "add", 100)
+
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "STRESS", "remove", 100)
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "ALCOHOL", "remove", 100)
+
   if not hasBeenRevived then
     TriggerEvent('tpz_core:isPlayerRespawned')
   end
