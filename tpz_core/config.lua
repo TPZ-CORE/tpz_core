2c43e902e3ec87ebcd045c1656c143d1bd00c971
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-26 22:20:42 +0300

    Update config.lua

--- tpz_core/config.lua
+++ tpz_core/config.lua
@@ -414,18 +414,4 @@ Config.Commands = {
         DiscordRoles  = { 670899926783361024 },
     },
 
-    ['reloadtargetcharacter'] = { 
-
-        Suggestion = "Perform this command to re-load the character of the selected player id.",
-
-        Webhook = { 
-            Enabled = false, 
-            Url = "", 
-            Color = 10038562,
-        },
-        
-        Groups  = { 'admin', 'mod' },
-        DiscordRoles  = { 670899926783361024 },
-    },
-
 }
