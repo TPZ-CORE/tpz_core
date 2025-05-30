3a6078b84b5f6658c4e1c1caac2731b3f0284c8a
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-30 08:10:31 +0300

    Update tp-server_commands.lua

--- tpz_core/server/tp-server_commands.lua
+++ tpz_core/server/tp-server_commands.lua
@@ -95,6 +95,68 @@ RegisterCommand("setgroup", function(source, args, rawCommand)
 
 end, false)
 
+--[[ Set Max Chars Command ]] --
+RegisterCommand("setmaxchars", function(source, args, rawCommand)
+   local _source = source
+   
+   local hasAcePermissions           = HasPermissionsByAce("tpzcore.setmaxchars", _source)
+   local hasAdministratorPermissions = hasAcePermissions
+
+   if not hasAcePermissions then
+      hasAdministratorPermissions = HasAdministratorPermissions(_source, Config.Commands['setmaxchars'].Groups, Config.Commands['setmaxchars'].DiscordRoles)
+   end
+
+   if hasAcePermissions or hasAdministratorPermissions then
+
+        local xPlayer = PlayerData[_source]
+
+        local target, chars = args[1], args[2]
+
+        local identifier      = xPlayer.identifier
+        local ip              = GetPlayerEndpoint(_source)
+
+        local discordIdentity = GetIdentity(_source, "discord")
+        local discordId       = string.sub(discordIdentity, 9)
+
+        local steamName       = GetPlayerName(_source)
+        local targetSteamName = GetPlayerName(tonumber(target))
+
+        if chars == nil or chars == '' or tonumber(chars) == nil then
+            TriggerClientEvent('tpz_core:sendRightTipNotification', _source, Locales['INVALID_SYNTAX'], 3000)
+            return
+        end
+
+        local webhookData = Config.Commands['setjob'].Webhook
+
+        if webhookData.Enabled then
+            local title   = "📋` /setmaxchars ".. target .. " " .. chars .. "`"
+            local message = "**Steam name: **`" .. steamName .. " (" .. xPlayer.group .. ")`**\nIdentifier**`" .. identifier .. "` \n**Discord:** <@" .. discordId .. ">**\nIP: **`" .. ip .. "`\n **Action:** `Used Set Max Characters Command`"
+            SendToDiscordWebhook(webhookData.Url, title, message, webhookData.Color)
+        end
+
+        if targetSteamName then
+            local tPlayer    = PlayerData[tonumber(target)]
+
+            if tPlayer then
+                
+                SetUserMaxCharacters(tonumber(target), chars)
+
+                TriggerClientEvent('tpz_core:sendRightTipNotification', _source, string.format("The following ID: %s, max characters set to: %s", target, chars), 3000)
+                TriggerClientEvent('tpz_core:sendRightTipNotification', tonumber(target), string.format("Your max characters has been set to: %s", chars), 3000)
+
+            else
+                TriggerClientEvent('tpz_core:sendRightTipNotification', _source, Locales['PLAYER_NOT_ONLINE'], 3000)
+            end
+
+        else
+            TriggerClientEvent('tpz_core:sendRightTipNotification', _source, Locales['PLAYER_NOT_ONLINE'], 3000)
+        end
+    else
+        TriggerClientEvent('tpz_core:sendRightTipNotification', _source, Locales['NO_PERMISSIONS'], 3000)
+    end
+    
+end, false)
+
 --[[ Set Job Command ]] --
 RegisterCommand("setjob", function(source, args, rawCommand)
    local _source = source
