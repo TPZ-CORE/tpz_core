203a4afaf58193be046dc16d44dcafd641f82a3f
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-03 20:54:39 +0300

    Create tp-server_webhooks.lua

---
+++ tpz_core/server/tp-server_webhooks.lua
@@ -0,0 +1,25 @@
+----------------------------------------------
+-- Webhooks (ADD ALL YOUR WEBHOOKS HERE)
+----------------------------------------------
+
+local WEBHOOKS = {
+    ["TPZ_MINING"] = "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+}
+
+----------------------------------------------
+-- Functions (DONT TOUCH WITHOUT EXPERIENCE)
+----------------------------------------------
+
+GetWebhookUrlByName = function(webhook)
+    if webhook = nil then
+        print("The webhook input seems to be null (invalid).")
+        return "n/a"
+    end
+    
+    if WEBHOOKS[webhook] == nil then
+        print(string.format("The webhook input %s is not registered on WEBHOOKS table."), webhook )
+        return "n/a"
+    end
+    
+    return WEBHOOKS[webhook]
+end
