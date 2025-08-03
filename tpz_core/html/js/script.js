323a70a4bcb77074b8217b2c756f2ff17836a669
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-08-03 20:04:54 +0300

    Add files via upload

---
+++ tpz_core/html/js/script.js
@@ -0,0 +1,51 @@
+
+function ResetAll() {
+  $("#announcement").hide();
+}
+
+document.addEventListener('DOMContentLoaded', function() { 
+  $("#main").fadeOut(); 
+  ResetAll();
+
+}, false);
+
+function CloseNUI() {
+  $("#main").fadeOut();
+  ResetAll();
+	$.post('http://tpz_core/close', JSON.stringify({}));
+}
+
+$(function() {
+
+	window.addEventListener('message', function(event) {
+		
+    var item = event.data;
+
+		if (item.type == "enable") {
+			document.body.style.display = item.enable ? "block" : "none";
+
+    } else if (item.action == 'sendAnnouncement') {
+      
+      $("#announcement-title").text(item.title);
+      $("#announcement-title-description").text(item.title_description);
+
+      let title_rgba = item.title_rgba != null ? item.title_rgba : "rgba(247, 247, 247, 0.911)";
+      let title_description_rgba = item.title_description_rgba != null ? item.title_description_rgba :  "rgba(171, 170, 170, 0.91)";
+
+      $("#announcement-title").css('color', title_rgba);
+      $("#announcement-title-description").css('color', title_description_rgba);
+
+      $("#announcement").show();
+      $("#main").fadeIn(1000);
+
+      var audio = new Audio('./audio/chime.mp3');
+      audio.volume = 0.7;
+      audio.play();
+
+    } else if (item.action == "close") {
+      CloseNUI();
+    }
+
+  });
+
+});
