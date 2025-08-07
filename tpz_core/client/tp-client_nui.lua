a9ce22046ff95ae8e9c7942f5ffd2f31f5c1834f
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-08-07 23:31:20 +0300

    Update tp-client_nui.lua

--- tpz_core/client/tp-client_nui.lua
+++ tpz_core/client/tp-client_nui.lua
@@ -1,11 +1,21 @@
+local NUIFocus = false
 
 -----------------------------------------------------------
 --[[ Local Functions ]]--
 -----------------------------------------------------------
 
-ToggleUI = function(display)
+local ToggleUI = function(display)
+
+    NUIFocus = display 
+
 	SetNuiFocus(false,false)
     SendNUIMessage({ type = "enable", enable = display })
+end
+
+local FormatTime = function (seconds)
+    local minutes = math.floor(seconds / 60)
+    local secs = seconds % 60
+    return string.format("%02d:%02d", minutes, secs)
 end
 
 -----------------------------------------------------------
@@ -26,6 +36,30 @@ function SendAnnouncement(title, description, duration, title_rgba, description_
 
     Wait(duration)
     CloseNUI()
+end
+
+function DisplayDeathCountdown(countdown)
+
+    countdown = FormatTime(countdown)
+
+    if tonumber(countdown) and tonumber(countdown) <= 0 then
+        countdown = Locales['DEATH_NUI_RESPAWN_NOW']
+    end
+
+    if not NUIFocus then
+        SendNUIMessage({ 
+            action            = 'displayDeathCountdown',
+            title             = Locales['DEATH_NUI_TITLE'],
+            title_description = Locales['DEATH_NUI_DESCRIPTION'],
+            countdown         = countdown,
+        })
+    
+        ToggleUI(true)
+
+    else
+        SendNUIMessage({ action = 'updateDeathCountdown', countdown = countdown })
+    end
+
 end
 
 function CloseNUI()
