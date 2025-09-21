27df2854c0f2367b0f687cfbdef01df9d0245da8
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-21 22:01:34 +0300

    Update init.lua

--- tpz_core/client/init.lua
+++ tpz_core/client/init.lua
@@ -14,17 +14,17 @@ function core.RequestModuleAwait(module)
         while module == nil or module and not module.loaded() do
             Citizen.Wait(50)
         end
-
-        callback(module)
     end)
 
 end
 
 function core.IsModuleLoaded(module, callback)
     Citizen.CreateThread(function()
-        while module == nil do
+
+        while module == nil or module and not module.loaded() do
             Citizen.Wait(50)
         end
+
         callback(module)
     end)
 end
