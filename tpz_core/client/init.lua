6c3f0ece85adea6f9ba31b4989c117a7339288be
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-21 22:01:10 +0300

    Update init.lua

--- tpz_core/client/init.lua
+++ tpz_core/client/init.lua
@@ -8,6 +8,18 @@ InvokeNative    = Citizen.InvokeNative
 --[[ Functions ]]--
 -----------------------------------------------------------
 
+function core.RequestModuleAwait(module)
+
+    Citizen.CreateThread(function()
+        while module == nil or module and not module.loaded() do
+            Citizen.Wait(50)
+        end
+
+        callback(module)
+    end)
+
+end
+
 function core.IsModuleLoaded(module, callback)
     Citizen.CreateThread(function()
         while module == nil do
