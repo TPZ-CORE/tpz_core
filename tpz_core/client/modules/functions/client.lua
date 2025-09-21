aa9c7c12fe50b91a956cf2ece4f08dcf04fbd1be
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-22 01:05:31 +0300

    Add files via upload

---
+++ tpz_core/client/modules/functions/client.lua
@@ -0,0 +1,276 @@
+core.functions = {}
+
+core.stop = function()
+    core.functions = nil
+end
+
+-----------------------------------------------------------
+--[[ General Utility Functions  ]]--
+-----------------------------------------------------------
+
+Citizen.CreateThread(function()
+
+    core.functions.LoadTexture = function(hash)
+        if not HasStreamedTextureDictLoaded(hash) then
+            RequestStreamedTextureDict(hash, true)
+            while not HasStreamedTextureDictLoaded(hash) do
+                Wait(1)
+            end
+            return true
+        end
+        return false
+    end
+    
+    
+    core.functions.BigInt  = function(text)
+        local string1 = DataView.ArrayBuffer(16)
+        string1:SetInt64(0, text)
+        return string1:GetInt64(0)
+    end
+    
+    core.functions.DrawText  = function(text, font, x, y, fontscale, fontsize, r, g, b, alpha, textcentred, shadow)
+        local str = CreateVarString(10, "LITERAL_STRING", text)
+        SetTextScale(fontscale, fontsize)
+        SetTextColor(r, g, b, alpha)
+        SetTextCentre(textcentred)
+        if shadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+        SetTextFontForCurrentCommand(font)
+        DisplayText(str, x, y)
+    end
+    
+    core.functions.GetWebhookUrl = function(webhook)
+        local wait = true
+        local data_result
+    
+        TriggerEvent("tpz_core:ExecuteServerCallBack", "tpz_core:getWebhookUrl", function(cb) data_result = cb wait = false end, { webhook = webhook } )
+    
+        while wait do
+            Wait(1)
+        end
+    
+        return data_result
+    end
+    
+    core.functions.CloseMapUI = function()
+        InvokeNative(0x2FF10C9C3F92277E,GetHashKey('MAP'))
+    end
+    
+    core.functions.GetTableLength = function(T)
+        local count = 0
+        for _ in pairs(T) do count = count + 1 end
+        return count
+    end
+    
+    core.functions.StartsWith = function(inputString, findString)
+        return string.sub(inputString, 1, string.len(findString)) == findString
+    end
+
+    core.functions.Round = function(num, numDecimalPlaces)
+        local mult = 10^(numDecimalPlaces or 0)
+        return math.floor(num * mult + 0.5) / mult
+    end
+
+    --- Performs linear interpolation between two values.
+    ---@param a number (The starting value)
+    ---@param b number (The ending value)
+    ---@param t number (The interpolation factor, usually between 0 and 1)
+    ---@return number (The interpolated value between a and b)
+    core.functions.Lerp = function(a, b, t)
+      return a + (b - a) * t
+    end
+    
+    core.functions.Split = function(inputstr, sep)
+        if sep == nil then
+            sep = "%s"
+        end
+
+        local t = {}
+
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            table.insert(t, str)
+        end
+
+        return t
+
+    end
+
+    -----------------------------------------------------------
+    --[[ Player & Ped Utility Functions  ]]--
+    -----------------------------------------------------------
+
+    core.functions.GetNearestPlayers = function(distance)
+        local closestDistance = distance
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed, true, true)
+        local closestPlayers = {}
+    
+        for _, player in pairs(GetActivePlayers()) do
+            local target = GetPlayerPed(player)
+    
+            if target ~= playerPed then
+                local targetCoords = GetEntityCoords(target, true, true)
+                local distance = #(targetCoords - coords)
+    
+                if distance < closestDistance then
+                    table.insert(closestPlayers, player)
+                end
+            end
+        end
+        return closestPlayers
+    end
+
+    core.functions.PlayAnimation = function(ped, anim)
+    
+        if not DoesAnimDictExist(anim.dict) then
+            return false
+        end
+        
+        local await = 10000
+        local loaded = true
+    
+        RequestAnimDict(anim.dict)
+        
+        while not HasAnimDictLoaded(anim.dict) do
+    
+            await = await - 10
+    
+            if await <= 0 then
+                loaded = false
+                break
+            end
+    
+            Citizen.Wait(10)
+        end
+    
+        if loaded then
+            TaskPlayAnim(ped, anim.dict, anim.name, anim.blendInSpeed, anim.blendOutSpeed, anim.duration, anim.flag, anim.playbackRate, false, false, false, '', false)
+            RemoveAnimDict(anim.dict)
+        end
+    
+        return loaded
+    end
+    
+    core.functions.LoadModel = function(model)
+        local model = GetHashKey(inputModel)
+ 
+        RequestModel(model)
+
+        local await = 10000
+        local loaded = true
+
+        while not HasModelLoaded(model) do 
+            RequestModel(model)
+
+            await = await - 10
+
+            if await <= 0 then
+                loaded = false
+                break
+            end
+
+            Citizen.Wait(10)
+        end
+
+        return loaded
+    end
+    
+    core.functions.RemoveEntityProperly = function(entity, objectHash)
+    	DeleteEntity(entity)
+    	DeletePed(entity)
+    	SetEntityAsNoLongerNeeded( entity )
+    
+    	if objectHash then
+    		SetModelAsNoLongerNeeded(objectHash)
+    	end
+    end
+    
+    core.functions.HealPlayer = function()
+        local ped = PlayerPedId()
+    
+        InvokeNative(0xC6258F41D86676E0, ped, 0, 100) -- inner first
+        SetEntityHealth(ped, 600, 1) -- outter after
+        InvokeNative(0xC6258F41D86676E0, ped, 1, 100) -- fills inner
+        InvokeNative(0x675680D089BFA21F, ped, 1065330373) -- fills outter.
+    end
+    
+    core.functions.TeleportToCoords = function(x, y, z, heading)
+        local playerPedId = PlayerPedId()
+        SetEntityCoords(playerPedId, x, y, z, true, true, true, false)
+    
+        if heading then
+            SetEntityHeading(playerPedId, heading)
+        end
+    
+        while not HasCollisionLoadedAroundEntity(PlayerPedId()) do
+            Wait(500)
+        end
+    
+    end
+    
+    core.functions.TeleportPedToCoords = function(ped, x, y, z, heading)
+        local playerPedId = ped or PlayerPedId()
+        SetEntityCoords(playerPedId, x, y, z, true, true, true, false)
+    
+        if heading then
+            SetEntityHeading(playerPedId, heading)
+        end
+    
+        while not HasCollisionLoadedAroundEntity(PlayerPedId()) do
+            Wait(500)
+        end
+    
+    end
+    
+    core.functions.TeleportToWaypoint = function()
+    
+        local ped = PlayerPedId()
+        local GetGroundZAndNormalFor_3dCoord = GetGroundZAndNormalFor_3dCoord
+        local waypoint = IsWaypointActive()
+        local coords = GetWaypointCoords()
+        local x, y, groundZ, startingpoint = coords.x, coords.y, 650.0, 750.0
+        local found = false
+    
+        if not waypoint then
+            return
+        end
+        DoScreenFadeOut(500)
+        Wait(1000)
+        FreezeEntityPosition(ped, true)
+        for i = startingpoint, 0, -25.0 do
+            local z = i
+            if (i % 2) ~= 0 then
+                z = startingpoint + i
+            end
+            SetEntityCoords(ped, x, y, z - 1000)
+            Wait(1000)
+            found, groundZ = GetGroundZAndNormalFor_3dCoord(x, y, z)
+            if found then
+                SetEntityCoords(ped, x, y, groundZ)
+                FreezeEntityPosition(ped, false)
+                Wait(1000)
+                DoScreenFadeIn(650)
+                break
+            end
+        end
+    end
+    
+    core.functions.GetPlayerData = function()
+    
+        local wait = true
+        local data_result
+    
+        TriggerEvent("tpz_core:ExecuteServerCallBack", "tpz_core:getPlayerData", function(cb)
+            data_result = cb
+            wait = false
+        end)
+    
+        while wait do
+            Wait(25)
+        end
+    
+        return data_result
+    
+    end
+
+    core.SetModuleLoaded("functions")
+end)
---
+++ tpz_core/client/player/playerdeath.lua
@@ -0,0 +1,452 @@
+
+local DeathData = { cooldown = 0, cooldown_alert = 0, isDead = false, cameraHandler = nil, cameraAngleY = 0.0, cameraAngleZ = 0.0}
+
+local Prompts       = GetRandomIntInRange(0, 0xffffff)
+local PromptsList   = {}
+
+-----------------------------------------------------------
+--[[ Prompts  ]]--
+-----------------------------------------------------------
+
+AddEventHandler('onResourceStop', function(resourceName)
+  if (GetCurrentResourceName() ~= resourceName) then
+    return
+  end
+
+  if Config.OnPlayerDeath.DeathCamera then
+    ClearDeathCam()
+  end
+  
+  if Config.OnPlayerDeath.DeathCameraEffects then
+
+    for index, effect in pairs (Config.OnPlayerDeath.DeathCameraEffects) do
+      AnimpostfxStop(effect)
+    end
+  
+  end
+  
+end)
+
+RegisterDeathPromptActions = function()
+
+  for index, tprompt in pairs (Config.OnPlayerDeath.PromptKeys) do
+
+    if tprompt.enabled then
+
+      -- We are getting the label of the prompt and the key.
+      local str      = tprompt.label
+      local keyPress = tprompt.key
+    
+      -- We registering the prompt.
+      local dPrompt = PromptRegisterBegin()
+      PromptSetControlAction(dPrompt, keyPress)
+      str = CreateVarString(10, 'LITERAL_STRING', str)
+      PromptSetText(dPrompt, str)
+
+      -- When registering, we set the respawn key as not enabled since we use a cooldown system.
+      if index == 'RESPAWN' then
+        PromptSetEnabled(dPrompt, 0)
+      else
+        PromptSetEnabled(dPrompt, 1)
+      end
+
+      PromptSetVisible(dPrompt, 1)
+      PromptSetStandardMode(dPrompt, 1)
+      PromptSetHoldMode(dPrompt, tprompt.holdMode) -- we set the hold mode
+      PromptSetGroup(dPrompt, Prompts)
+      Citizen.InvokeNative(0xC5F428EE08FA7F2C, dPrompt, true)
+      PromptRegisterEnd(dPrompt)
+    
+      table.insert(PromptsList, {prompt = dPrompt, action = index})
+    end
+
+  end
+
+end
+
+-----------------------------------------------------------
+--[[ Events  ]]--
+-----------------------------------------------------------
+
+RegisterNetEvent('tpz_core:resurrectPlayer')
+AddEventHandler('tpz_core:resurrectPlayer', function(cb)
+
+  ResurrectPlayer(nil, cb)
+
+  DeathData.cooldown       = 0
+  DeathData.cooldown_alert = 0
+  DeathData.isDead         = false
+
+end)
+
+RegisterNetEvent('tpz_core:applyLethalDamage')
+AddEventHandler('tpz_core:applyLethalDamage', function()
+  SetEntityHealth(PlayerPedId(), 0, 0)
+end)
+
+RegisterNetEvent('tpz_core:onPlayerRespawn')
+AddEventHandler('tpz_core:onPlayerRespawn', function()
+  DeathData.cooldown       = 0
+  DeathData.cooldown_alert = 0
+end)
+
+
+-----------------------------------------------------------
+--[[ Threads  ]]--
+-----------------------------------------------------------
+
+-- Registering the death prompt actions.
+Citizen.CreateThread(function()
+
+  RegisterDeathPromptActions() 
+
+  while true do
+
+    Wait(0)
+
+    local sleep = true
+    local player = PlayerPedId() -- call it only once
+
+    if IsEntityDead(player) then
+      sleep = false
+
+      -- Performing actions only once.
+      if not DeathData.isDead then
+
+        DeathData.isDead = true
+
+        exports.spawnmanager.setAutoSpawn(false)
+        TriggerServerEvent('tpz_core:savePlayerDeathStatus', 1)
+
+        NetworkSetInSpectatorMode(false, player)
+        DisplayRadar(false)
+
+        DeathData.cooldown = Config.OnPlayerDeath.PromptKeys['RESPAWN'].cooldown
+
+        if Config.OnPlayerDeath.DisplayDeathNUI then
+          DisplayDeathCountdown(DeathData.cooldown)
+        end
+
+        if Config.OnPlayerDeath.DeathCamera then
+          StartDeathCam()
+        end
+
+        if Config.OnPlayerDeath.DeathCameraEffects then
+
+          for index, effect in pairs (Config.OnPlayerDeath.DeathCameraEffects) do
+            AnimpostfxPlay(effect)
+          end
+        end
+
+      end
+
+      -- The following prompts such as respawn will be visible only if the player is not being carried.
+      if DeathData.isDead and not IsEntityAttachedToAnyPed(player) then
+
+        ProcessCamControls()
+
+        local notifyText = string.format(Config.OnPlayerDeath.BottomPromptLabelDisplays['WHILE_RESPAWN_HAS_COOLDOWN'], DeathData.cooldown) 
+
+        if DeathData.cooldown <= 0 then
+          notifyText = Config.OnPlayerDeath.BottomPromptLabelDisplays['WHILE_NO_RESPAWN_COOLDOWN']
+        end
+
+        if Config.OnPlayerDeath.DisplayDeathNUI then 
+          notifyText = ''
+        end
+
+        local label = CreateVarString(10, 'LITERAL_STRING', notifyText)
+        PromptSetActiveGroupThisFrame(Prompts, label)
+
+        for index, prompt in pairs(PromptsList) do
+
+          if prompt.action == "RESPAWN" then
+            local state = DeathData.cooldown <= 0 and 1 or 0
+            PromptSetEnabled(prompt.prompt, state)
+          end
+
+          if prompt.action == "ALERT" then
+            local state = DeathData.cooldown_alert <= 0 and 1 or 0
+            PromptSetEnabled(prompt.prompt, state)
+          end
+
+          if PromptHasHoldModeCompleted(prompt.prompt) then
+
+            if prompt.action == "RESPAWN" then
+              
+              CloseNUI()
+
+              DoScreenFadeOut(3000)
+
+              Wait(3000)
+              OnPlayerRespawn()
+
+            elseif prompt.action == "ALERT" then
+              TriggerServerEvent("tpz_medics:server:alert", true)
+              DeathData.cooldown_alert = Config.OnPlayerDeath.PromptKeys["ALERT"].cooldown
+            end
+
+            Wait(2000)
+
+          end
+
+        end
+
+
+      end
+
+    end
+
+    if sleep then
+      Citizen.Wait(1000)
+    end
+
+  end
+
+end)
+
+-- Cooldown system when the player is dead.
+Citizen.CreateThread(function ()
+  while true do
+    Wait(1000)
+
+    if DeathData.cooldown > 0 then
+
+      DeathData.cooldown = DeathData.cooldown - 1
+
+      if DeathData.cooldown <= 0 then
+        DeathData.cooldown = 0
+      end
+
+      if Config.OnPlayerDeath.DisplayDeathNUI then
+        DisplayDeathCountdown(DeathData.cooldown)
+      end
+
+    end
+
+    if DeathData.cooldown_alert > 0 then
+
+      DeathData.cooldown_alert = DeathData.cooldown_alert - 1
+
+      if DeathData.cooldown_alert <= 0 then
+        DeathData.cooldown_alert = 0
+      end
+
+    end
+
+  end
+
+end)
+
+-----------------------------------------------------------
+--[[ Functions  ]]--
+-----------------------------------------------------------
+
+OnPlayerRespawn = function()
+
+  local player    = PlayerPedId()
+  local pedCoords = GetEntityCoords(player)
+
+  local closestDistance = math.huge
+  local closestLocation = nil
+  local spawnedCoords   = nil
+
+  for _, location in pairs(Config.OnPlayerDeath.RespawnLocations) do
+
+    local locationCoords = vector3(location.Coords.x, location.Coords.y, location.Coords.z)
+    local currentDistance = #(pedCoords - locationCoords)
+
+    if currentDistance <= closestDistance then
+      closestDistance = currentDistance
+      closestLocation = location.Town
+      spawnedCoords   = location.Coords
+    end
+
+  end
+
+  ResurrectPlayer(spawnedCoords, false)
+ -- TriggerServerEvent("tpzcore:getPlayerSkin")
+end
+
+
+local keepdown = false
+
+ResurrectPlayer = function(currentHospital, hasBeenRevived)
+
+  local player = PlayerPedId()
+
+  TriggerServerEvent('tpz_core:savePlayerDeathStatus', 0)
+  DeathData.isDead   = false
+  DeathData.cooldown = 0
+
+  if Config.OnPlayerDeath.DisplayDeathNUI then
+    CloseNUI()
+  end
+
+  ResurrectPed(player)
+
+  Wait(200)
+
+  if Config.OnPlayerDeath.DeathCamera then
+    ClearDeathCam()
+  end
+
+  if Config.OnPlayerDeath.DeathCameraEffects then
+
+    for index, effect in pairs (Config.OnPlayerDeath.DeathCameraEffects) do
+      AnimpostfxStop(effect)
+    end
+
+  end
+
+  if currentHospital then -- set entitycoords with heading
+    Citizen.InvokeNative(0x203BEFFDBE12E96A, player, currentHospital.x, currentHospital.y, currentHospital.z, currentHospital.heading, false, false, false)
+  end
+
+    TriggerServerEvent("tpz_characters:requestCharacterSkin") -- requests skin reload.
+  
+  Wait(2000)
+
+  if Config.OnPlayerDeath.RagdollOnResurrection and not hasBeenRevived then
+
+    TriggerServerEvent('tpz_core:onPlayerDeathContents')
+
+    keepdown = true
+    CreateThread(function() -- tread to keep player down
+        while keepdown do
+          Wait(0)
+          SetPedToRagdoll(PlayerPedId(), 4000, 4000, 0, 0, 0, 0)
+          ResetPedRagdollTimer(PlayerPedId())
+        end
+    end)
+         
+    local innerHealth = Citizen.InvokeNative(0x36731AC041289BB1, PlayerPedId(), 0)
+
+    if innerHealth then
+      SetEntityHealth(PlayerPedId(), Config.OnPlayerDeath.HealthOnRespawn + innerHealth)
+    end
+
+    if Config.OnPlayerDeath.PlayAnimPostFXPlay then
+      AnimpostfxPlay("Title_Gen_FewHoursLater")
+    end
+
+    Wait(3000)
+
+    DoScreenFadeIn(2000)
+
+    if Config.OnPlayerDeath.PlayAnimPostFXPlay then
+      AnimpostfxPlay("PlayerWakeUpInterrogation") 
+    end
+
+    DisplayRadar(true)
+
+    Wait(Config.OnPlayerDeath.RagdollKeepDown)
+    keepdown = false
+
+  else
+    DoScreenFadeIn(2000) 
+
+    local innerHealth = Citizen.InvokeNative(0x36731AC041289BB1, PlayerPedId(), 0)
+
+    if innerHealth then
+      SetEntityHealth(PlayerPedId(), Config.OnPlayerDeath.HealthOnRespawn + innerHealth)
+    end
+
+  end
+
+  TriggerEvent('tpz_core:onPlayerRespawn')
+
+  -- tpz_metabolism
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "HUNGER", "add", 100)
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "THIRST", "add", 100)
+
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "STRESS", "remove", 100)
+  TriggerEvent("tpz_metabolism:setMetabolismValue", "ALCOHOL", "remove", 100)
+
+  if not hasBeenRevived then
+    TriggerEvent('tpz_core:isPlayerRespawned')
+  end
+
+end
+
+StartDeathCam = function()
+  ClearFocus()
+  local playerPed = PlayerPedId()
+
+  DeathData.cameraHandler = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", GetEntityCoords(playerPed), 0, 0, 0, GetGameplayCamFov())
+  SetCamActive(DeathData.cameraHandler, true)
+  RenderScriptCams(true, true, 1000, true, false)
+
+end
+
+ClearDeathCam = function()
+  NetworkSetInSpectatorMode(false, PlayerPedId())
+  ClearFocus()
+
+  RenderScriptCams(false, false, 0, true, false)
+  DestroyCam(DeathData.cameraHandler, false)
+  DeathData.cameraHandler = nil
+end
+
+
+ProcessCamControls = function()
+  local newPos = nil
+
+  if Config.OnPlayerDeath.UseControlsCamera then
+
+      local playerPed = PlayerPedId()
+      local pCoords = GetEntityCoords(playerPed)
+
+      newPos = ProcessNewPosition()
+
+      SetCamCoord(DeathData.cameraHandler, newPos.x, newPos.y, newPos.z)
+      PointCamAtCoord(DeathData.cameraHandler, pCoords.x, pCoords.y, pCoords.z + 0.5)
+  else
+      newPos = GetEntityCoords(PlayerPedId())
+  end
+
+end
+
+ProcessNewPosition = function()
+    local mouseX = 0.0
+    local mouseY = 0.0
+    if (IsInputDisabled(0)) then
+        mouseX = GetDisabledControlNormal(1, 0x4D8FB4C1) * 1.5
+        mouseY = GetDisabledControlNormal(1, 0xFDA83190) * 1.5
+    else
+        mouseX = GetDisabledControlNormal(1, 0x4D8FB4C1) * 0.5
+        mouseY = GetDisabledControlNormal(1, 0xFDA83190) * 0.5
+    end
+    DeathData.cameraAngleZ = DeathData.cameraAngleZ - mouseX
+    DeathData.cameraAngleY = DeathData.cameraAngleY + mouseY
+
+    if (DeathData.cameraAngleY > 89.0) then DeathData.cameraAngleY = 89.0 elseif (DeathData.cameraAngleY < -89.0) then DeathData.cameraAngleY = -89.0 end
+    local pCoords = GetEntityCoords(PlayerPedId())
+    local behindCam = {
+        x = pCoords.x + ((Cos(DeathData.cameraAngleZ) * Cos(DeathData.cameraAngleY)) + (Cos(DeathData.cameraAngleY) * Cos(DeathData.cameraAngleZ))) / 2 * (3.0 + 0.5),
+        y = pCoords.y + ((Sin(DeathData.cameraAngleZ) * Cos(DeathData.cameraAngleY)) + (Cos(DeathData.cameraAngleY) * Sin(DeathData.cameraAngleZ))) / 2 * (3.0 + 0.5),
+        z = pCoords.z + ((Sin(DeathData.cameraAngleY))) * (3.0 + 0.5)
+    }
+    local rayHandle = StartShapeTestRay(pCoords.x, pCoords.y, pCoords.z + 0.5, behindCam.x, behindCam.y, behindCam.z, -1
+        , PlayerPedId(), 0)
+    local a, hitBool, hitCoords, surfaceNormal, entityHit = GetShapeTestResult(rayHandle)
+
+    local maxRadius = 3.0
+    if (hitBool and Vdist(pCoords.x, pCoords.y, pCoords.z + 0.5, hitCoords) < 3.0 + 0.5) then
+        maxRadius = Vdist(pCoords.x, pCoords.y, pCoords.z + 0.5, hitCoords)
+    end
+
+    local offset = {
+        x = ((Cos(DeathData.cameraAngleZ) * Cos(DeathData.cameraAngleY)) + (Cos(DeathData.cameraAngleY) * Cos(DeathData.cameraAngleZ))) / 2 * maxRadius,
+        y = ((Sin(DeathData.cameraAngleZ) * Cos(DeathData.cameraAngleY)) + (Cos(DeathData.cameraAngleY) * Sin(DeathData.cameraAngleZ))) / 2 * maxRadius,
+        z = ((Sin(DeathData.cameraAngleY))) * maxRadius
+    }
+
+    local pos = {
+        x = pCoords.x + offset.x,
+        y = pCoords.y + offset.y,
+        z = pCoords.z + offset.z
+    }
+
+    return pos
+end
