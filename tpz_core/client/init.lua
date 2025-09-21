0f69d6afb941edeaa0d494199433664dabb9e415
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-21 21:02:37 +0300

    Update init.lua

--- tpz_core/client/init.lua
+++ tpz_core/client/init.lua
@@ -5,3 +5,15 @@ Await = Citizen.Await
 InvokeNative = Citizen.InvokeNative
 
 core = {}
+
+function core.onResourceStop()
+    -- todo, nothing. 
+end
+
+-- Event: only runs once when resource stops
+AddEventHandler('onResourceStop', function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then
+        return
+    end
+    core.onResourceStop()
+end)
