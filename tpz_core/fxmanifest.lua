0d4056516c1f47a04bd0d4b1b28bd7a464a2d9af
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-22 01:04:53 +0300

    Add files via upload

--- tpz_core/fxmanifest.lua
+++ tpz_core/fxmanifest.lua
@@ -4,12 +4,26 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE Base'
-version '1.0.7'
+version '1.0.5'
 
 ui_page 'html/index.html'
 
-shared_scripts { 'config.lua', 'locales.lua'   }
-client_scripts { 'client/*.lua' 'client/modules/*.lua', 'client/nui/*.lua', 'client/api/*.lua'}
+shared_scripts { 'config.lua', 'locales.lua' }
+
+client_scripts { 
+    'init.lua',
+    'client/*.lua',
+	"client/modules/**/*shared.lua",
+	"client/modules/**/*client.lua",
+    "client/modules/component/clothesList.lua",
+	"client/modules/component/horseComponents.lua",
+	"client/modules/component/data/horseComponents.lua",
+	"client/modules/component/data/playerComponents.lua",
+    'client/player/*.lua',
+    'client/nui/*.lua',
+    'client/api/*.lua'
+}
+
 server_scripts { 'server/*.lua' }
 
 files { 'html/**/*' }
---
+++ tpz_core/init.lua
@@ -0,0 +1,53 @@
+core           = {}
+
+CreateThreadNow = Citizen.CreateThreadNow
+Await           = Citizen.Await
+InvokeNative    = Citizen.InvokeNative
+
+local LoadedModules = {}
+
+-----------------------------------------------------------
+--[[ Functions ]]--
+-----------------------------------------------------------
+
+function core.RequestModuleAwait(module)
+
+    Citizen.CreateThread(function()
+        while LoadedModules[module] == nil do
+            Citizen.Wait(100)
+        end
+    end)
+
+end
+
+function core.IsModuleLoaded(module, callback)
+    Citizen.CreateThread(function()
+
+        while LoadedModules[module] == nil  do
+            Citizen.Wait(100)
+        end
+
+        callback(module)
+    end)
+end
+
+function core.SetModuleLoaded(module)
+    LoadedModules[module] = true
+end
+
+function core.stop()
+    LoadedModules = nil
+end
+
+-----------------------------------------------------------
+--[[ Events  ]]--
+-----------------------------------------------------------
+
+-- Event: only runs once when resource stops
+AddEventHandler('onResourceStop', function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then
+        return
+    end
+
+    core.stop()
+end)
