7daa7f86ee90773870b7e02bacc99290db3fb76a
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-11-12 23:47:50 +0200

    Update taskmanager.lua

--- tpz_core/client/taskmanager.lua
+++ tpz_core/client/taskmanager.lua
@@ -1,19 +1,23 @@
-local TaskManager = { list = {} }
+local TaskManager = { list = {}, count = 0 }
 
 -- Register script as busy / not. 
 function TaskManager:SetBusy(scriptName, state)
 
-    -- We remove from the list if busy state is false
-    -- this allows us to not loop through many data. 
-    if state then self.list[scriptName] = state else self.list[scriptName] = nil end
+    if state and self.list[scriptName] == nil then
+        self.count += 1
+        self.list[scriptName] = true
+    end
+
+    if not state and self.list[scriptName] then
+        self.count = math.max(0, self.count - 1)
+        self.list[scriptName] = nil
+    end
+
 end
 
 -- Check if any script action is busy.
 function TaskManager:IsPlayerBusy()
-    for _, busy in pairs(self.list) do
-        if busy then return true end
-    end
-    return false
+    return self.count > 0
 end
 
 exports('IsPlayerBusy', function() return TaskManager:IsPlayerBusy() end)
