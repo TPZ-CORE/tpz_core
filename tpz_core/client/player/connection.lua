bfbc4afe31f61ba5c590172eab2c352ae06e1bcf
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-11-25 12:21:21 +0200

    Update connection.lua

--- tpz_core/client/player/connection.lua
+++ tpz_core/client/player/connection.lua
@@ -1,40 +1,16 @@
-local DESYNC_STATUS = false
-
-local MenuData = {}
-
-TriggerEvent("tpz_menu_base:getData", function(call)
-    MenuData = call
-end)
 
 --[[ ------------------------------------------------
    Events
 ]]---------------------------------------------------
 
-RegisterNetEvent("tpz_core:client:desync")
-AddEventHandler("tpz_core:client:desync", function(isDesync)
+RegisterNetEvent('tpz_core:isPlayerReady')
+AddEventHandler("tpz_core:isPlayerReady", function(newChar)
 
-    if isDesync == true and DESYNC_STATUS ~= isDesync then 
-
-        SetNuiFocus(false, false)
-        MenuData.CloseAll()
-
-    end
-
-    DESYNC_STATUS = isDesync
-
-end)
-
-if Config.KickPlayerOnEthernetDisconnect.Enabled then
-
-    RegisterNetEvent('tpz_core:isPlayerReady')
-    AddEventHandler("tpz_core:isPlayerReady", function(newChar)
-    
-        Citizen.CreateThread(function()
-            while true do
-                TriggerServerEvent("tpz_core:server:heartbeat")
-                Wait(500)
-            end
-        end)
+    Citizen.CreateThread(function()
+        while true do
+            TriggerServerEvent("tpz_core:server:heartbeat")
+            Wait(500)
+        end
     end)
     
-end
+end)
