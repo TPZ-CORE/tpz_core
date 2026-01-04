ee1034f304f292a5adae0ba7889e2e09eed7bc52
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-01-04 13:09:49 +0200

    Update webhooks.lua

--- tpz_core/server/discord/webhooks.lua
+++ tpz_core/server/discord/webhooks.lua
@@ -2,29 +2,37 @@
 -- Webhooks (ADD ALL YOUR WEBHOOKS HERE)
 ----------------------------------------------
 
+/*
+   Insert all the webhooks on the scripts you are having below.
+
+   All of our webhooks are located here and not through the script config, the configuration file is shared,
+   and since it is shared, a RedM Executer can find all of your script webhooks easily and spam or share them to others.
+*/
+
+
 local WEBHOOKS = {
-    ["TPZ_MINING"]     = "xxxxxxxxxxxxxxxxxxxxxxxxxxx", -- this is an example.
-    ["TPZ_LUMBERJACK"] = "xxxxxxxxxxxxxxxxxxxxxxxxxxx", -- this is an example.
-    ["DEVTOOLS"]       = "xxxxxxxxxxxxxxxxxxxxxxxxxxx",  -- this is an example.
+
+    ["tpz_lumberjack"] = {
+        ['DEVTOOLS_INJECTION_CHEAT'] = "",
+    },
+
+    ["tpz_mining"] = {
+        ['DEVTOOLS_INJECTION_CHEAT'] = "",
+    },
+
+
 }
 
 ----------------------------------------------
 -- Functions (DONT TOUCH WITHOUT EXPERIENCE)
 ----------------------------------------------
 
-GetWebhookUrlByName = function(webhook)
-    if webhook == nil then
-        print("The webhook input seems to be null (invalid).")
-        return "n/a"
-    end
-    
-    if WEBHOOKS[webhook] == nil then
-        print(string.format("The webhook input %s is not registered on WEBHOOKS table.", webhook ))
-        return "n/a"
-    end
+GetWebhookUrlByName = function(script, webhooktype)
 
-    webhook = WEBHOOKS[webhook]
-
-    local encoded = EncodeHexString(webhook, "0x0x0-")
-    return tostring(encoded)
+    if WEBHOOKS[script][webhooktype] == nil then 
+        print(string.format('Attempted to retrieve GetWebhookUrlByName from an non valid webhook type of the mentioned script: { script: %s, type: %s }', script, webhooktype))
+        return "N/A"
+    end
+   
+    return WEBHOOKS[script][webhooktype]
 end
